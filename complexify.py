dd5c4e56addd45d8feec6c1bbd84ab67929eea1d
Gerry Munn <14248894+Gerry2k5@users.noreply.github.com>
2022-08-23 23:03:28 +0100

    Tidy up of parameters and basic classify function

--- complexify.py
+++ complexify.py
@@ -15,8 +15,8 @@ char_symbols.extend(list(range(123, 127)))
 # Remove symbols which should not be used as replacements, to avoid
 # characters which may be difficult to use on console sessions
 # as well as minimising the risk of causing SQL errors on broken systems
-sym_donotuse = [34, 39, 92, 96, 124]
-char_symbols = list(set(char_symbols) - set(sym_donotuse))
+symbols_donotuse = [32, 34, 39, 92, 96, 124]
+char_symbols = list(set(char_symbols) - set(symbols_donotuse))
 
 
 def main():
@@ -25,7 +25,7 @@ def main():
     default_basestring = ""
     # Collect stdin only if something has been passed (to prevent blocking)
     if not sys.stdin.isatty():
-        default_basestring = sys.stdin.readlines()
+        default_basestring = "".join(sys.stdin.readlines()).rstrip("\n")
 
     parser = argparse.ArgumentParser(description="Replace characters in a string to make it more complex")
     parser.add_argument(
@@ -39,12 +39,6 @@ def main():
         action="help"
     )
     parser.add_argument(
-        "-n", "--numchars",
-        type=int,
-        default=default_char_count,
-        help="Number of characters to replace"
-    )
-    parser.add_argument(
         "-i", "--ignore",
         nargs="?",
         const="",
@@ -52,6 +46,21 @@ def main():
         help="characters to ignore (Defaults to '" + default_ignore_chars + "')"
     )
     args = parser.parse_args()
+    print(args)
+
+    # TODO: It may be possible to convert this check to an argparse Action
+    if len(args.basestring) < 4:
+        print("Base String must be at least 4 characters")
+        exit(1)
+
+    string_dict = classify(args.basestring)
+
+
+def classify(base_string):
+    # Build dicts containing incidences of each character class in string
+    # with the index of that character in the string as the key to the dict
+    for index in range(len(base_string)):
+        print("{} {}".format(index, base_string[index]))
 
 
 if __name__ == "__main__":
